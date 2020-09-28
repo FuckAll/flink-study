29e16e08fa3832cd8772784a4fdb2b72ae8a3e56
FuckAll <wxdanobody2011>
2020-09-28 08:14:30 +0800

    增加WindowAll

--- src/main/java/org/apache/flink/operators/WindowAll.java
+++ src/main/java/org/apache/flink/operators/WindowAll.java
@@ -13,10 +13,15 @@ import org.apache.flink.util.Collector;
  * @date: 27/09/2020
  * @description:
  */
-public class TumblingWindow {
+public class WindowAll {
 
-    // 结论：Window是一个时间窗口，分为：TumblingWindow(翻滚窗口), SlidingWindow(滚动窗口)，SessionWindow(会话窗口)
+    // 结论：WindowAll是Non-Keyed Window；窗口分为：Non-Keyed Windows 和 Key Windows；Key Windows有keyBy， Non-Keyed Windows没有。
+    // Keyed Windows 使用window, Non-Keyed Windows使用windowAll;
     // TumblingWindow的运行方式：每隔指定时间,指定下一个窗口；例如：每隔5秒，执行5秒内窗口的数据，例如23:50:05 执行 23:50:00 - 23:50:05的数据。
+
+    // Keyed Windows: 调用keyBy就是keyedStream,keyedStream 能保证相同的key在同一个分区里，这时候进行窗口运算是有意义的，所以能够保证并发处理.
+    // Non-Keyed Windows: 没有keyBy的支持，即使是拆分为多个并行，进行窗口运算，最终依旧要汇总成一个结果，所以Non-Keyed Windows支持一个并行。
+    // 参考：http://www.wendaoxueyuan.com/thy-post/detail/ce9d4ce9-691a-4214-87d5-f2766b3e3d66
     // 启动：nc -l 9999 （nc命令在本地监听9999接口）
     // 输入：(10秒内)
     // one,1
@@ -24,14 +29,13 @@ public class TumblingWindow {
     // two,3
     // two,2
     // 输出：
-    //   one,2
     //   two,3
     //
     // 解释：1. one,1 进入，此时窗口内存在(one,1)，但是由于时间未到，不执行;
     //      2. one,2 进入，此时窗口内存在(one,2)，但是由于时间未到，不执行;
     //      3. two,3 进入，此时窗口内存在(two,3)，但是由于时间未到，不执行;
     //      4. two,2 进入，此时窗口内存在(two,2)，但是由于时间未到，不执行;
-    //      5. 时间到达10s，由于keyBy, one和two在不同的分区，分别执行，one中最大的是(one,2)，two中最大的是(two,3)
+    //      5. 时间到达10s，keyBy无效，WindowAll是Non-Keyed Window, 找到最大的3，输出：（two，3）
 
     public static void main(String[] args) throws Exception {
         //创建流运行环境
@@ -47,7 +51,7 @@ public class TumblingWindow {
                     System.out.println("invalid input:" + value);
                 }
             }
-        }).keyBy(0).windowAll(TumblingProcessingTimeWindows.of(Time.seconds(10))).maxBy(1).print();
+        }).windowAll(TumblingProcessingTimeWindows.of(Time.seconds(10))).maxBy(1).print();
         env.execute();
     }
 }
