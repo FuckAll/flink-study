28018e1a0c6bc9a611b0b8aee4e7d5bcd43b9879
FuckAll <wxdanobody2011>
2020-09-28 09:20:41 +0800

    增加CountWindow和SlidingWindow

---
+++ src/main/java/org/apache/flink/operators/SlidingWindow.java
@@ -0,0 +1,51 @@
+package org.apache.flink.operators;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+/**
+ * @author: izgnod.
+ * @date: 27/09/2020
+ * @description:
+ */
+public class SlidingWindow {
+
+    // 结论：SlidingWindows 分为Non-Keyed Window和Keyed Windows；Key Windows有keyBy，进行分区内计算； Non-Keyed Windows没有keyBy，只能在一个分区内计算。
+    // SlidingWindows有两个基本参数，第一个参数：窗口大小，第二个参数：触发时间，例如slidingWindow(10,1), 时间窗口是10s内，触发时间是1s; 即每隔1秒，计算10s之内的数据。
+    // 注意：SlidingWindow和TumblingWindow是不一样的，SlidingWindow第一个窗口跟启动的时间有关。TumblingWindow跟启动时间无关，例如5秒，那么1分钟会在05,10,15......这些时间点进行计算。
+    // >>> 23:50:05 计算 23:49:55 - 23:50:05
+    // >>> 23:50:06 计算 23:49:56 - 23:50:06
+    // >>> 23:50:07 计算 23:49:57 - 23:50:07
+    // >>> ......
+
+    // Keyed Windows: 调用keyBy就是keyedStream,keyedStream 能保证相同的key在同一个分区里，这时候进行窗口运算是有意义的，所以能够保证并发处理.
+    // Non-Keyed Windows: 没有keyBy的支持，即使是拆分为多个并行，进行窗口运算，最终依旧要汇总成一个结果，所以Non-Keyed Windows支持一个并行。
+    // 参考：http://www.wendaoxueyuan.com/thy-post/detail/ce9d4ce9-691a-4214-87d5-f2766b3e3d66
+    // 启动：nc -l 9999 （nc命令在本地监听9999接口）
+
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<String> stream = env.socketTextStream("127.0.0.1", 9999);
+        SingleOutputStreamOperator<Tuple2<String, Integer>> singleOutputStreamOperator = stream.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                String[] split = value.split(",");
+                if (split.length == 2) {
+                    out.collect(new Tuple2<>(split[0], Integer.parseInt(split[1])));
+                } else {
+                    System.out.println("invalid input:" + value);
+                }
+            }
+        });
+        singleOutputStreamOperator.windowAll(SlidingProcessingTimeWindows.of(Time.seconds(10), Time.seconds(1))).maxBy(1).print("non-keyed-slidingWindow");
+        singleOutputStreamOperator.keyBy(0).window(SlidingProcessingTimeWindows.of(Time.seconds(10), Time.seconds(1))).maxBy(1).print("keyed-slidingWindow");
+        env.execute();
+    }
+}
